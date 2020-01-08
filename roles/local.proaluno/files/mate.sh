a2e2191079324f83dc78fa709c50d8560ccee6c9
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2020-01-08 20:22:52 -0300

    Finalizando customizações do mate
    Fix #30
    Fix #29
    Fix #26
    Fix #23
    Fix #21
    Fix #10

--- roles/local.proaluno/files/mate.sh
+++ roles/local.proaluno/files/mate.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 dconf load / < /proaluno/dconf-dump.ini
+
+mate-settings-daemon --replace &
+mate-panel --replace &
+marco --no-composite --replace &
+killall mate-volume-control-applet
+killall caja
+mate-volume-control-applet &
+killall compton
+compton &
