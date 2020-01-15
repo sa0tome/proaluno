2cb238cdb4e308a9f9a339e6d4200790d00ec46f
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2020-01-14 21:24:20 -0300

    mate config loaded via x11

--- roles/local.proaluno/files/mate.sh
+++ roles/local.proaluno/files/mate.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-dconf load / < /proaluno/dconf-dump.ini
+#dconf load / < /proaluno/dconf-dump.ini
 
-mate-settings-daemon --replace &
-mate-panel --replace &
-marco --no-composite --replace &
-killall mate-volume-control-applet
-killall caja
-mate-volume-control-applet
-
-# as vezes o menu não carrega na primeira
-dconf load / < /proaluno/dconf-dump.ini
+#mate-settings-daemon --replace &
+#mate-panel --replace &
+#marco --no-composite --replace &
+#killall mate-volume-control-applet
+#killall caja
+#mate-volume-control-applet
