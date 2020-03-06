6fa339b2bf7b8c8de948f378c86a1d763b93c81d
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2020-03-06 15:33:10 -0300

    Update mate.sh

--- roles/local.proaluno/files/mate.sh
+++ roles/local.proaluno/files/mate.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Tentando desabilitar tela de descanço
-# https://ubuntu-mate.community/t/stop-the-screen-from-blanking-in-ubuntu/678/3
-xset s off
-
 /proaluno/countdown.sh &
 
 conky -d -p 5
