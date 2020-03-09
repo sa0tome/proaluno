452a915e0e2dcce17ba782cc5ed7333b6776b360
saotome <57924632+sa0tome@users.noreply.github.com>
2020-03-09 09:31:52 -0300

    Update welcome.sh

--- roles/local.proaluno/files/welcome.sh
+++ roles/local.proaluno/files/welcome.sh
@@ -5,8 +5,22 @@ zenity --warning --no-wrap --text \
 <span size="x-large" weight="bold">
     Pró-Aluno   
 </span>
-
 <span>
-    Você está na pró-aluno Faculdade de Filosofia, Letras e Ciências Humanas!
+Para imprimir:
+1) Baixe o arquivo. Nunca mande impressões direto do navegador.
+2) Escolha entre as impressoras de frente e verso com borda menor
+ou maior, ou somente um lado. Essa será a configuração do seu documento.
+3) Selecione quantas páginas por lado você quer. Se for duas, vá em 
+"Configurar página" e, em "Páginas por lado", selecione 2. 
+4) Se seu pdf foi escaneado na horizontal, em "Configurar página", não 
+esqueça de selecionar a orientação "Paisagem".
+5) Por fim, vá em "Manuseio da página" e selecione o redimensionamento 
+"Preencher a área de impressão".
+6) Mande para imprimir!
+7) Para confirmar se seu arquivo foi para a fila de impressão, vá na Área 
+de Trabalho e clique em "Veja suas quotas". Ali, você pode verificar quantas 
+cotas você tem no dia e se seu documento foi para o processamento. Quando seu
+documento for para a fila, ele aparecerá como "active". 
+
 </span>
 '
